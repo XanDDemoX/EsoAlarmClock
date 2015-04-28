52b10cb8f1c504579715c00c5fb54484d26c0cd1
XanDDemoX <xanddemox@gmail.com>
2015-04-28 20:42:34 +0100

    Version 0.0.3. Implemented sound. Fixed UI error messages related to command parsing. Updated Readme

--- AlarmClock/AlarmClock.lua
+++ AlarmClock/AlarmClock.lua
@@ -1,10 +1,11 @@
 -----------------------------------
---    Alarm Clock Version 0.0.2  --
+--    Alarm Clock Version 0.0.3  --
 -----------------------------------
 
 local _prefix ="[Alarm Clock]: "
 local _settings = { enabled = true, alarms={} }
 local _cmds = {}
+local _defaultAlarm = {message="[No Message]", duration=-1, interval=-15, sound=-1, sid="-NONE"}
 
 local function isOnString(str)
 	str = string.lower(str)
@@ -20,7 +21,9 @@ local function stringNilOrEmpty(str)
 	return str == nil or str == ""
 end
 
-local function stringSplit(str,pattern)
+local function stringSplit(pattern,str)
+	if str == nil then return {} end
+	
 	local split ={}
 	
 	for s in string.gmatch(str,pattern) do
@@ -28,6 +31,14 @@ local function stringSplit(str,pattern)
 	end
 	
 	return split
+end
+
+local function GetSound(id)
+	id = string.upper(id)
+	if id == "NONE" then
+		return id, math.abs(_defaultAlarm.sound)
+	end
+	return id,SOUNDS[id]
 end
 
 local function GetCurrentTime()
@@ -39,12 +50,13 @@ local function GetCurrentTime()
 end
 
 local function ParseCmd(str,callback)
-	local split = stringSplit(str,"%S+")
+	local split = stringSplit("%S+",str)
 	if callback == nil then return unpack(split) end
 	return callback(unpack(split))
 end
 
 local function TryParseTime(str)
+	if str == nil then return false end
 	local hour,mins = string.match(str,"(%d+):(%d+)")
 	if hour == nil or mins == nil then return false end 
 	hour = tonumber(hour)
@@ -81,37 +93,110 @@ local function Pack(...)
 	return tbl,count
 end
 
-local function SetAlarm(strTime,...)
+local function GetUpdateValue(newValue,current,default,newFormatted)
+	return ((newValue == current or newValue == default) and current) or (newFormatted or newValue)
+end
 
+local function UpdateAlarm(alarm,arg,default)
+	if alarm == nil or arg == nil then return end
+	
+	for k,v in pairs(alarm) do
+		if k == "interval" or k == "duration" then
+			alarm[k] = GetUpdateValue(arg[k],v,default[k],math.abs(arg[k]))
+		elseif k == "sound" then
+			alarm[k] = GetUpdateValue(arg[k],v,default[k],(arg[k] == default[k] and math.abs(arg[k])) )
+		elseif k == "sid" then 
+			alarm[k] = GetUpdateValue(arg[k],v,default[k],(arg[k] == default[k] and string.sub(arg[k],2,string.len(arg[k])-1) ))
+		else
+			alarm[k] = GetUpdateValue(arg[k],v,default[k])
+		end
+		
+	end	
+	
+	alarm.sound = GetUpdateValue(arg.sound,alarm.sound,default[k])
+	
+	return alarm
+end
+
+local function FormatSoundId(id)
+	if id ~= _defaultAlarm.sid then return id end
+	return string.sub(id,2,string.len(id))
+end
+
+local function SetAlarm(strTime,...)
+	-- start parse params
 	local parsed,hour,mins = TryParseTime(strTime)
 	
 	if parsed == false then return end
 	
 	local arg,count = Pack(...)
-
-	local iparsed,interval = TryParseNumber(arg[count],5,60,15)
-	local dparsed,duration = TryParseNumber(arg[count-1],1,5)
 	
-	if iparsed == true then table.remove(arg) end
-	if dparsed == true then 
+	local defaultMessage,defaultInterval,defaultDurtaion,defaultSound = "[No Message]",15,1,0
+
+	local intStr = arg[count]
+	local iparsed,interval = TryParseNumber(intStr,5,60,defaultInterval)
+	local dparsed,duration = TryParseNumber(arg[count-1],1,5,defaultDurtaion)
+	local sound,sid
+	
+	if iparsed == true then 
 		table.remove(arg) 
-	elseif iparsed == true then
-		duration = Clamp(interval,1,5)
-		interval = 15
+	else
+		sid,sound = GetSound(arg[count])
+		if sound ~= nil then
+			table.remove(arg)
+		end
+	end
+	
+	if dparsed == true and iparsed == true then 
+		table.remove(arg) 
+	elseif dparsed == false and iparsed == true then
+		duration = Clamp(tonumber(intStr),1,5)
+		interval = _defaultAlarm.interval
+	elseif sound == nil then
+		sid,sound= GetSound(arg[count])
+		if sound ~= nil then
+			table.remove(arg)
+		end
+	end
+	
+	if dparsed == false and iparsed == false then
+		duration = _defaultAlarm.duration
+		interval = _defaultAlarm.interval
 	end
 	
 	local message = table.concat(arg," ")
-	
+
 	local alarms = _settings.alarms
 	local str = GetTimeString(hour,mins)
 
-	if stringNilOrEmpty(message) == true then
-		message = "[No Message]"
+	if sound == nil then
+		sid,sound = GetSound(message)
+		if sound ~= nil then
+			message = defaultMessage
+		end
 	end
 	
-	alarms[str] = {message=message, duration=duration, interval=interval}
+	if stringNilOrEmpty(message) == true or message == sid then
+		message = defaultMessage
+	end
+
+	if sound == nil then 
+		sound,sid = _defaultAlarm.sound,_defaultAlarm.sid
+	end
+	-- end parse 
 	
-	d(_prefix.."Alarm set ["..str.."] "..message.." for "..tostring(duration).." minute(s) every "..tostring(interval).." seconds")
+	--create or update alarm
+	local alarm = alarms[str]
+	if alarm == nil then 
+		alarm = {message = message, duration=math.abs(duration), interval=math.abs(interval),sound=sound,sid=sid}
+		alarm = UpdateAlarm(alarm,{message=message, duration=duration, interval=interval, sound=sound,sid=sid},_defaultAlarm)
+		alarms[str] = alarm
+		d(_prefix.."Alarm set ["..str.."] "..alarm.message.." ["..FormatSoundId(alarm.sid).."] for "..tostring(alarm.duration).." minute(s) every "..tostring(alarm.interval).." seconds")
+	else
+		alarms[str] = UpdateAlarm(alarm,{message=message, duration=duration, interval=interval, sound=sound,sid=sid},_defaultAlarm)
+		d(_prefix.."Alarm updated ["..str.."] "..alarm.message.." ["..FormatSoundId(alarm.sid).."] for "..tostring(alarm.duration).." minute(s) every "..tostring(alarm.interval).." seconds")
+	end
+
 end
 
 local function ClearAlarm(strTime,silent)
@@ -145,12 +230,35 @@ local function TryGetAlarm(hour,mins)
 	return alarm
 end
 
-local function ShowAlert(...)
+local function ShowAlert(sound,...)
+	if sound == nil then return end
+	
 	local arg,count = Pack(...)
+	
 	if count < 1 then return end 
+	
+	local nsnd = tonumber(sound)
+	
+	if nsnd == nil then 
+		local sid,snd = GetSound(sound)
+		
+		if snd == nil then 
+			table.insert(arg,1,sound)
+			snd = _defaultAlarm.sound
+		end
+	end
+	
+	if snd == _defaultAlarm.sound then
+		snd = 0
+	end
+	
 	local msg = table.concat(arg," ")
-	CENTER_SCREEN_ANNOUNCE:DisplayMessage(CSA_EVENT_LARGE_TEXT,0,msg)
-	ZO_Alert(UI_ALERT_CATEGORY_ALERT,0,msg)
+	
+	CENTER_SCREEN_ANNOUNCE:AddMessage(999,CSA_EVENT_LARGE_TEXT,snd,msg)
+	
+	ZO_Alert(UI_ALERT_CATEGORY_ALERT,snd,msg)
+	
+	d(_prefix..msg)
 end
 
 local function SetTimeouts(func,interval,duration)
@@ -168,10 +276,7 @@ local function TriggerAlarm(strTime,alarm)
 		
 		local msg ="["..strTime.."] "..alarm.message
 		
-		local func = function()
-			ShowAlert(msg)
-			d(_prefix..msg)
-		end
+		local func = function() ShowAlert(alarm.sid,msg) end
 
 		SetTimeouts(func, alarm.interval * 1000, alarm.duration * 60000)
 		
@@ -200,11 +305,57 @@ local function CheckTime()
 
 end
 
+local function HandleSound(strTime,id)
+
+	local parsed,hour,mins = TryParseTime(strTime)
+	
+	if parsed == true then
+		if id == nil then return end 
+		
+		local alarm = TryGetAlarm(hour,mins)
+		if alarm == nil then return end
+		local sid
+		sid,id = GetSound(id)
+		if id == nil then return end 
+		alarm.sound = id
+		d(_prefix.."Alarm sound set ["..strTime.."] "..alarm.message.." ["..sid.."]")
+		return
+	else
+		id = strTime
+	end
+
+	if stringNilOrEmpty(id) == false then
+		local strId = string.upper(id)
+		id = SOUNDS[string.upper(strId)]
+		if id == nil then return end
+		PlaySound(id)
+		d(_prefix.."Playing sound "..strId)
+	else
+		local keys = _soundKeys
+		if keys == nil then 
+			keys = {}
+			for k,v in pairs(SOUNDS) do
+				table.insert(keys,k)
+			end
+			table.sort(keys)
+			_soundKeys = keys
+		end
+		d("[Sounds]")
+		for i,v in ipairs(keys) do
+			d(v)
+		end
+	end
+end
+
 local function Initialise()
+
+	local _soundKeys
 
 	_cmds["set"] = SetAlarm
 	_cmds["clear"] = ClearAlarm
 	_cmds["alert"] = ShowAlert
+	_cmds["sound"] = HandleSound
+	
 	
 	SLASH_COMMANDS["/alarm"] = function(arg)
 		
--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# EsoAlarmClock v0.0.2
-An addon for The Elder Scrolls Online for displaying a notification at a particular time. The duration and interval between notifications can be optionally configured.
+# EsoAlarmClock v0.0.3
+An addon for The Elder Scrolls Online for displaying a notification at a particular time optionally playing a sound. The duration and between notifications can be optionally configured.
 
-The notification is displayed in the standard notification areas in the center of the screen, the top right hand corner of the screen and the chat window.
+The notification is displayed in the standard notification areas in the center of the screen, the top right hand corner of the screen and the chat window. 
 
 Alarms are stored in the SavedVariables config file to preserve alarms between game sessions. Alarms are cleared once they have been triggered.
 
 Installation
 =============
 
-Place the "AlarmClock" folder in your addons folder:
+1. Download Zip by clicking the "Download Zip" button on the right
+2. Open Zip and go into the folder named "EsoAlarmClock-master"
+3. Extract or copy the "AlarmClock" folder  in your addons folder:
 
 "Documents\Elder Scrolls Online\live\Addons"
 
@@ -23,11 +25,16 @@ For example:
 Usage
 =============
 
-* /alarm set hour:min [messageString] [durationMinutes] [intervalSeconds] - Set an alarm at the specified time. Optionally set a message, the duration (1-5 minutes) and interval (5-60 seconds) can also be set.
-* /alarm set 15:30 Example Alarm 2 10 - Example sets an alarm at 15:30 with the text "Example Alarm" which will be displayed every 10 seconds for 2 minutes.
+* /alarm set [hour:min] [messageString] [durationMinutes] [intervalSeconds] [soundId] - Set an alarm at the specified time. Optionally set a message, the duration (1-5 minutes) and interval (5-60 seconds) can also be set.
+* /alarm set 15:30 Example Alarm 2 10 SKILL_GAINED - Example sets an alarm at 15:30 with the text "Example Alarm" which will be displayed every 10 seconds for 2 minutes playing the "SKILL_GAINED" sound.
 
 * /alarm clear hour:min - Clears the alarm at the specified time.
 * /alarm clear - Clears all alarms.
+
+* /alarm sound - Output all sound ids to chat window
+* /alarm sound [soundId] - Plays the sound of the given sound ids
+* /alarm sound [hour:min] [soundId] - Sets the specified alarm's sound to the specified soundId
+
 
 DISCLAIMER
 =============
